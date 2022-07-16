ecbf26c9fc4939a4bb3947b6c4091a828a5143ab
Modenero Support <support@modenero.com>
2022-07-16 08:04:05 -0400

    Rename monitor to monitoring.

---
+++ monitoring/src/views/HomeView.vue
@@ -0,0 +1,133 @@
+<template>
+    <main>
+        <section class="bg-gray-50 pt-12 sm:pt-16">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="max-w-4xl mx-auto text-center">
+                    <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">Trusted by developers from over 80 planets</h2>
+                    <p class="mt-3 text-xl text-gray-500 sm:mt-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus repellat laudantium.</p>
+                </div>
+            </div>
+            <div class="mt-10 pb-12 bg-white sm:pb-16">
+                <div class="relative">
+                    <div class="absolute inset-0 h-1/2 bg-gray-50"></div>
+                    <div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div class="max-w-4xl mx-auto">
+                            <dl class="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
+                                <div class="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
+                                    <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Pepperoni</dt>
+                                    <dd class="order-1 text-5xl font-extrabold text-indigo-600">100%</dd>
+                                </div>
+                                <div class="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
+                                    <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Delivery</dt>
+                                    <dd class="order-1 text-5xl font-extrabold text-indigo-600">24/7</dd>
+                                </div>
+                                <div class="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
+                                    <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Calories</dt>
+                                    <dd class="order-1 text-5xl font-extrabold text-indigo-600">100k</dd>
+                                </div>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section class="relative bg-gray-900">
+            <div class="h-80 w-full absolute bottom-0 xl:inset-0 xl:h-full">
+                <div class="h-full w-full xl:grid xl:grid-cols-2">
+                    <div class="h-full xl:relative xl:col-start-2">
+                        <img
+                        class="h-full w-full object-cover opacity-25 xl:absolute xl:inset-0"
+                        src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
+                        alt="People working on laptops"
+                        />
+                        <div aria-hidden="true" class="absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-gray-900 xl:inset-y-0 xl:left-0 xl:h-full xl:w-32 xl:bg-gradient-to-r"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8 xl:grid xl:grid-cols-2 xl:grid-flow-col-dense xl:gap-x-8">
+                <div class="relative pt-12 pb-64 sm:pt-24 sm:pb-64 xl:col-start-1 xl:pb-24">
+                    <h2 class="text-sm font-semibold text-indigo-300 tracking-wide uppercase">Valuable Metrics</h2>
+                    <p class="mt-3 text-3xl font-extrabold text-white">Get actionable data that will help grow your business</p>
+                    <p class="mt-5 text-lg text-gray-300">
+                        Rhoncus sagittis risus arcu erat lectus bibendum. Ut in adipiscing quis in viverra tristique sem. Ornare feugiat viverra eleifend fusce orci in quis amet. Sit in et vitae tortor, massa. Dapibus laoreet amet lacus nibh
+                        integer quis. Eu vulputate diam sit tellus quis at.
+                    </p>
+                    <div class="mt-12 grid grid-cols-1 gap-y-12 gap-x-6 sm:grid-cols-2">
+                        <p>
+                            <span class="block text-2xl font-bold text-white">8K+</span>
+                            <span class="mt-1 block text-base text-gray-300"><span class="font-medium text-white">Companies</span> use laoreet amet lacus nibh integer quis.</span>
+                        </p>
+
+                        <p>
+                            <span class="block text-2xl font-bold text-white">25K+</span>
+                            <span class="mt-1 block text-base text-gray-300"><span class="font-medium text-white">Countries around the globe</span> lacus nibh integer quis.</span>
+                        </p>
+
+                        <p>
+                            <span class="block text-2xl font-bold text-white">98%</span>
+                            <span class="mt-1 block text-base text-gray-300"><span class="font-medium text-white">Customer satisfaction</span> laoreet amet lacus nibh integer quis.</span>
+                        </p>
+
+                        <p>
+                            <span class="block text-2xl font-bold text-white">12M+</span>
+                            <span class="mt-1 block text-base text-gray-300"><span class="font-medium text-white">Issues resolved</span> lacus nibh integer quis.</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- This example requires Tailwind CSS v2.0+ -->
+        <section class="fixed bottom-0 inset-x-0 pb-2 sm:pb-5">
+            <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                <div class="p-2 rounded-lg bg-indigo-600 shadow-lg sm:p-3">
+                    <div class="flex items-center justify-between flex-wrap">
+                        <div class="w-0 flex-1 flex items-center">
+                            <span class="flex p-2 rounded-lg bg-indigo-800">
+                                <!-- Heroicon name: outline/speakerphone -->
+                                <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"
+                                    />
+                                </svg>
+                            </span>
+                            <p class="ml-3 font-medium text-white truncate">
+                                <span class="md:hidden"> We announced a new product! </span>
+                                <span class="hidden md:inline"> Big news! We're excited to announce a brand new product. </span>
+                            </p>
+                        </div>
+                        <div class="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+                            <a href="#" class="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"> Learn more </a>
+                        </div>
+                        <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-2">
+                            <button type="button" class="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white">
+                                <span class="sr-only">Dismiss</span>
+                                <!-- Heroicon name: outline/x -->
+                                <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  components: {
+    // HelloWorld
+  }
+}
+</script>
